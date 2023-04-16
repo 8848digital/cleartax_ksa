dd7b5050ccf9896ea3d7f4432162184ca12243db
Sankara Subramanian <sankarsubramaniankvs@gmail.com>
2023-04-16 11:09:57 +0530

    fix: einv

--- cleartax_ksa/cleartax_ksa/API/einv.py
+++ cleartax_ksa/cleartax_ksa/API/einv.py
@@ -1,5 +1,5 @@
 import frappe
-import cleartax_ksa.utils as utils
+import cleartax_ksa.cleartax_ksa.utils as utils
 import json
 import requests 
 
@@ -14,7 +14,7 @@ def generate_einv(**kwargs):
             'company_address': utils.get_dict('Address',invoice.company_address),
             'customer_address': utils.get_dict('Address',invoice.customer_address),
             'item_list': [utils.get_dict('Item', row.item_code) for row in invoice.items],
-            'vat_settings': utils.get_dict('KSA VAT Settings',{'company':invoice.company})
+            'vat_settings': utils.get_dict('KSA VAT Setting',{'company':invoice.company})
         }
         return einv_request(data,invoice.name)
     except Exception as e:
@@ -25,11 +25,12 @@ def generate_einv(**kwargs):
 def einv_request(data,inv):
     try:
         settings = frappe.get_doc('Cleartax Settings')
-        url = settings.host_url + "/api/method/cleartax.cleartax.API.irn.generate_irn"
+        url = settings.host_url + "/api/method/cleartax_server_ksa.cleartax_server_ksa.API.einv.generate_einv"
         headers = {
             'sandbox': str(settings.sandbox),
             'Content-Type': 'application/json',
             'token': settings.get_password('sandbox_auth_token') if settings.sandbox else settings.get_password('production_auth_token'),
+            'device': settings.get_password('device_id')
         }
         payload = json.dumps(data, indent=4, sort_keys=False, default=str)
 
@@ -38,14 +39,11 @@ def einv_request(data,inv):
             response = response.json()['message']
             if response.get('error'):
                 return utils.error_response(response.get('error'))
-
             utils.response_logger(response['request'], response['response'], "GENERATE IRN", "Sales Invoice", inv, response['msg'])
-
             if response['msg'] == 'Success':
-                store_irn_details(inv,response['response'][0])
+                store_irn_details(inv,response['response'])
                 return utils.success_response()
-
-            return utils.response_error_handling(json.loads(json.dumps(response['response'][0])))
+            return utils.response_error_handling(json.loads(json.dumps(response['response'])))
 
 
     except Exception as e:
@@ -55,10 +53,14 @@ def einv_request(data,inv):
 @frappe.whitelist()
 def store_irn_details(inv,response):
     try:
+        frappe.logger('cleartax').exception(response)
         values = {
-                    'ksa_einv_qr': response.get('QRCode'),
+                    'ksa_qr': response.get('QRCode'),
+                    'raw_qr': response.get('RawQRCode'),
                     'generated_date': response.get('GeneratedDate'),
-                    'generated_time': response.get('GeneratedTime')
+                    'generated_time': response.get('GeneratedTime'),
+                    'invoice_hash': response.get('InvoiceHash'),
+                    'invoice_xml': response.get('InvoiceXml')
                 }
         frappe.db.set_value("Sales Invoice", inv, values)
         frappe.db.commit()
---
+++ cleartax_ksa/public/js/sales_invoice.js
@@ -0,0 +1,27 @@
+frappe.ui.form.on('Sales Invoice', {
+	refresh(frm) {
+		if (frm.selected_doc.docstatus == 1) {
+						cur_frm.add_custom_button(__("E-Invoice"), function () {
+							frappe.call({
+								method: "cleartax_ksa.cleartax_ksa.API.einv.generate_einv",
+								args: {
+									invoice: frm.selected_doc.name
+								},
+								callback: function (r) {
+								//console.log(r.message)
+									if (r.message.msg == 'success') {
+										frappe.msgprint("IRN Created Successfully!")
+										location.reload();
+									}
+									else {
+										// frappe.msgprint(r.message.error)
+										frappe.msgprint(r.message.error)
+									}
+								}
+							});
+						}, __('Create'));
+						cur_frm.page.set_inner_btn_group_as_primary(__('Create'));
+					}
+                }
+            });
+					
